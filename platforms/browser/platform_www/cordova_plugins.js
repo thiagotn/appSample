e566e4a561f78aa4056642abd70630ecb24a4589
Thiago Teixeira Nogueira <RM49824@fiap.com.br>
2016-04-09 15:13:31 +0100

    acelerometro

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,10 +1,65 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
+        "id": "cordova-plugin-camera.CameraProxy",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/src/browser/AccelerometerProxy.js",
+        "id": "cordova-plugin-device-motion.AccelerometerProxy",
+        "pluginId": "cordova-plugin-device-motion",
+        "runs": true
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-browsersync": "0.1.1"
+    "cordova-plugin-browsersync": "0.1.1",
+    "cordova-plugin-camera": "2.1.1",
+    "cordova-plugin-device-motion": "1.2.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -2,64 +2,172 @@
 $(document).on("mobileinit", function () {
     $.mobile.defaultPageTransition = "none";
     $.mobile.defaultDialogTransition = "none";
+    //usersListPage.init();
     communicator.init();
+    accelerometer.init();
 });
 
 var communicator = (function () {
-    console.log('entrou');
-
-    var obj = window.localStorage.getItem('messages');
-
     var savedMessages = [];
-    if (obj) savedMessages = JSON.parse(obj);
-
-    var messagesHtml = $('#messages');
+    var messageHtml = $("#message");
+    var messagesListHtml = $("#messages");
 
     var onSendMessage = function () {
-        console.log('envia mensagem');
-        var message = { text: $('#message').val() };
-
-        if (message.text == '') return;
-
-        savedMessages.push(message);
-
-        window.localStorage.setItem('messages', JSON.stringify(savedMessages));
-
-        $('#message').val('');
-
-        renderMessages(true);
-    };
+        saveMessage(messageHtml.val(), "text");
+        renderMessages("refresh");
+        messageHtml.val("");
+    }
 
     var onDeleteMessages = function () {
-        console.log('remove mensagem');
-
-        window.localStorage.clear();
-
+        localStorage.clear();
         savedMessages = [];
-        renderMessages();
-    };
+        renderMessages("refresh");
+        messageHtml.val("");
+    }
 
     var renderMessages = function (refresh) {
-        if (!savedMessages) {
-            $('#message').html('<li>no messages</li>');
-            return;
+        messagesListHtml.html("");
+        if (savedMessages.length <= 0) {
+            //"<li>No messages</li>"
+            createListItem("No messages");
+        } else {
+            savedMessages.forEach(function (data) {
+                if (data.type === "image64") {
+                    //<li><img src='data:image/png;base64, " + data.value + "'></img></li>"
+                    createListItem("data:image/png;base64, " + data.value, true);
+                } else {
+                    //"<li>" + data.value + "</li>"
+                    createListItem(data.value, false);
+                }
+            });
+        }
+        messagesListHtml.listview(refresh)
+    }
+
+    var createListItem = function (value, isImg) {
+        var clonedLi = null;
+        if (isImg) {
+            clonedLi = $("li[data-template-message-img='true']").clone().removeAttr("data-template-message-img").appendTo(messagesListHtml);
+            $(clonedLi.children()[0]).attr("src", value);
+        } else {
+            clonedLi = $("li[data-template-message-text='true']").clone().removeAttr("data-template-message-text").appendTo(messagesListHtml);
+            clonedLi.html(value);
+        }
+        return clonedLi;
+    }
+
+    var onGetPhoto = function () {
+        var options = {
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            destinationType: Camera.DestinationType.DATA_URL
+            //sourceType: Camera.PictureSourceType.CAMERA,
+            //destinationType: Camera.DestinationType.FILE_URI
         }
 
-        messagesHtml.html('');
-        savedMessages.forEach(function (msg) {
-            messagesHtml.append('<li>' + msg.text + '</li>');
-        });
+        navigator.camera.getPicture(onGetPhotoSuccess, onGetPhotoError, options);
+    }
 
-        if(refresh) messagesHtml.listview('refresh');
-    };
+    var onGetPhotoSuccess = function (imageData) {
+        saveMessage(imageData, "image64");
+        renderMessages("refresh");
+        messageHtml.val("");
+    }
+
+    var onGetPhotoError = function (messageError) {
+        console.log(messageError);
+    }
+
+    var saveMessage = function (data, type) {
+        savedMessages.push({ value: data, type: type });
+        localStorage.setItem("messages", JSON.stringify(savedMessages));
+    }
+
+    var loadMessages = function () {
+        if (localStorage.getItem("messages")) {
+            JSON.parse(localStorage.getItem("messages")).forEach(function (data) {
+                savedMessages.push({ value: data.value, type: data.type });
+            });
+            renderMessages();
+        }
+    }
 
     return {
         init: function () {
-            console.log('add tap');
-            $('#send-message').on("tap", onSendMessage);
-            $('#delete-messages').on("tap", onDeleteMessages);
-            $('#messages').listview().listview('refresh');
-            renderMessages(true);
+            $("#send-message").on("tap", onSendMessage);
+            $("#delete-messages").on("tap", onDeleteMessages);
+            $("#get-photo").on("taphold", onGetPhoto);
+            loadMessages();
         }
-    };
+    }
+})();
+
+var accelerometer = (function () {
+    var transforms = ["-webkit-transform", "-moz-transform", "-ms-transform", "transform"];
+
+    var accelerometerSuccess = function (data) {
+        var rotate = {};
+        transforms.forEach(function (attr) {
+            var rotateX = 'rotateX(' + (data.x * 100) + 'deg)';
+            var rotateY = 'rotateY(' + (data.y * 100) + 'deg)';
+            var rotateZ = 'rotateZ(' + (data.z * 100) + 'deg)';
+
+            rotate[attr] = rotateX + rotateY + rotateZ;
+        });
+
+        $("#box-phone-d1").css(rotate)
+    }
+
+    var accelerometerError = function (messageError) {
+        console.log(messageError);
+    }
+
+    return {
+        init: function () {
+            var options = { frequency: 2000 };
+            navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, options);
+        }
+    }
+})();
+
+var usersListPage = (function () {
+    var listUsers = $("#users");
+
+    var onGetUserListSuccess = function (data) {
+        listUsers.html("");
+        if (data && data.length <= 0) {
+            createListItem("No users", false);
+        } else {
+            data.forEach(function (user) {
+                //"<li>" + user.user + " - " + user.pass + "</li>"
+                createListItem(user, true);
+            });
+        }
+        listUsers.listview("refresh");
+    }
+
+    var createListItem = function (value, isUsers) {
+        var clonedLi = $("li[data-template-user='true']").clone().removeAttr("ata-template-user").appendTo(listUsers);
+        if (isUsers) {
+            var childrens = clonedLi.children();
+        } else {
+            clonedLi.html(value);
+        }
+    }
+
+    var getUserList = function () {
+        $.ajax({
+            dataType: 'jsonp',
+            jsonpCallback: 'jsonCallback',
+            contentType: 'application/json',
+            url: 'http://www.mocky.io/v2/56f0a2ef1000007f018ef257',
+            success: onGetUserListSuccess
+        });
+    }
+
+    return {
+        myKey: 'k',
+        init: function () {
+            getUserList();
+        }
+    }
 })();
